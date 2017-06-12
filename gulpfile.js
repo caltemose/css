b11adf43976a38975d5686beed5baabbe9efe624
Chad Altemose <chad@chadzilla.com>
2017-06-12 08:15:57 -0400

    gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require('gulp')
 const del = require('del')
 const pug = require('gulp-pug')
 const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+const browsersync = require('browser-sync').create()
+
+/* Supporting tasks
+---------------------------------------------------------------- */
 
 gulp.task('clean', (done) => {
     return del('dist', done)
@@ -16,10 +21,44 @@ gulp.task('html', () => {
 gulp.task('css', () => {
     return gulp.src('src/styles/main.sass')
         .pipe(sass().on('error', sass.logError))
-        // .pipe(autoprefixer())
+        .pipe(autoprefixer())
         .pipe(gulp.dest('dist/styles'))
-        // .pipe(browsersync.stream())
+        .pipe(browsersync.stream())
 })
 
+gulp.task('serve', () => {
+    browsersync.init({
+        server: {
+            baseDir: './dist'
+        },
+        port: 3333,
+        notify: false,
+        open: false
+    })
+})
+
+gulp.task('reload', (done) => {
+    browsersync.reload()
+    done()
+})
+
+/* Watch tasks
+---------------------------------------------------------------- */
+
+gulp.task('watch:html', () => {
+    gulp.watch('src/html/**/*', gulp.series('html', 'reload'))
+})
+
+gulp.task('watch:styles', () => {
+    gulp.watch('src/styles/**/*', gulp.series('css'))
+})
+
+gulp.task('watch', gulp.parallel('watch:html', 'watch:styles'))
+
+
+/* Primary tasks
+---------------------------------------------------------------- */
 
 gulp.task('build', gulp.series('clean', gulp.parallel('html', 'css')))
+
+gulp.task('default', gulp.series('build', gulp.parallel('serve', 'watch')))
